c2692ce45a77a3c7657dc24045c2653c634eb67d
SoumendraPrasadSahoo <somu123_pdp@yahoo.co.in>
2018-03-08 16:08:24 +0530

    2nd Commit

--- src/eApp_Automation/ExecuteTest.java
+++ src/eApp_Automation/ExecuteTest.java
@@ -1,7 +1,7 @@
 package eApp_Automation;
 
 import java.io.IOException;
-
+//Main method here. Execute test
 public class ExecuteTest {
 	public static void main(String args[]) throws IOException, InterruptedException{
 	login_console test = new login_console();
